79a64ad6c9d707b49266b839097ca478f43bb57e
Alessio-Tosoni <alessiotosoni988@gmail.com>
2023-11-27 17:17:51 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+
+import LaMiaPrimaAppConVite from './components/laMiaPrimaAppConVite.vue';
+export default {
+  components: {
+    LaMiaPrimaAppConVite
+  }
+}
+</script>
+
+<template>  
+  <LaMiaPrimaAppConVite/>
+</template>
